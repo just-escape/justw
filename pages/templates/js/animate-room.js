ccf17d8503b1946a8fe751a56f0a42646f59859b
Jean-Baptiste Braun <jb.braun@justescape.fr>
2022-03-03 00:18:23 +0100

    Minor SEO updates, remove puppeter's mansion

--- pages/templates/js/animate-room.js
+++ pages/templates/js/animate-room.js
@@ -44,49 +44,7 @@
       digimiamTL.play();
   }
 
-  var puppetIsAnimated = false;
-  var puppetTimeout;
-  var puppetTL = anime.timeline({
-      autoplay: false,
-      direction: 'alternate',
-      begin: function(anim) {
-          puppetIsAnimated = true;
-      },
-      complete: function(anim) {
-          puppetIsAnimated = false;
-          puppetTimeout = setTimeout(animatePuppet, 6000);
-      }
-  }).add({
-      targets: '.puppet-door',
-      scaleX: 0.85,
-      duration: 800,
-      easing: 'easeOutQuart'
-  }, 200).add({
-      targets: '.puppet-eyes',
-      translateY: -11,
-      duration: 800,
-      easing: 'easeOutQuart'
-  }, 200).add({
-      targets: '.puppet-knife',
-      translateX: 4,
-      translateY: 11,
-      duration: 500,
-      easing: 'linear'
-  }, 0);
-
-  document.querySelector('#puppet-svg').onmouseenter = function() {
-      if (!puppetIsAnimated) {
-          clearTimeout(puppetTimeout);
-          animatePuppet();
-      }
-  };
-
-  function animatePuppet() {
-      puppetTL.play();
-  }
-
   window.onload = function() {
         digimiamTimeout = setTimeout(animateDigimiam, 4000);
-        puppetTimeout = setTimeout(animatePuppet, 8000);
   }
 </script>
