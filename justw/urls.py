cb60cd0ac5f9367a6edcda68f264c581b4d397be
Jean-Baptiste Braun <jb.braun@justescape.fr>
2023-02-01 20:53:36 +0100

    Add new content

--- justw/urls.py
+++ justw/urls.py
@@ -33,6 +33,7 @@ urlpatterns = i18n_patterns(
     path('blog/salle-reunion-escape-game', views.blog_salle_reunion_escape_game, name='blog-salle-reunion-escape-game'),
     path('blog/seminaire-entreprise-escape-game', views.blog_seminaire_entreprise_escape_game, name='blog-seminaire-entreprise-escape-game'),
     path('blog/seminaire-entreprise-original', views.blog_seminaire_entreprise_original, name='blog-seminaire-entreprise-original'),
+    path('blog/salle-seminaire-lille-just-escape', views.blog_salle_seminaire_lille_just_escape, name='blog-salle-seminaire-lille-just-escape'),
     path('404', views.error404, name='404'),
     path('partenaires', views.partners, name='partners'),
     path('contact', views.contact, name='contact'),
--- pages/views.py
+++ pages/views.py
@@ -429,6 +429,11 @@ def blog_seminaire_entreprise_original(request):
     return render(request, 'blog_seminaire_entreprise_original.html', data)
 
 
+def blog_salle_seminaire_lille_just_escape(request):
+    data = {'lang': '', 'localized': False}
+    return render(request, 'blog_salle_seminaire_lille_just_escape.html', data)
+
+
 def error404(request):
     data = {'current_page': '404', 'noindex': True, 'lang': get_lang(request), 'localized': True}
     r = render(request, '404.html', data)
